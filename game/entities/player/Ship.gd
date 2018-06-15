2c69762ec6e09b150bd1069722fdb60e2f44c2c0
unwarysage <unwary@comcast.net>
2018-06-15 16:56:29 -0500

    initial player ship script

--- game/entities/player/Ship.gd
+++ game/entities/player/Ship.gd
@@ -1,13 +1,47 @@
 extends RigidBody2D
 
+export (float) var stat_engine_strength
+export (float) var stat_engine_heat
+export (float) var stat_thruster_strength
+export (float) var stat_thermal_buffer
 
+onready var engine_strength = stat_engine_strength
+onready var engine_heat = stat_engine_heat
+onready var thruster_strength = stat_thruster_strength
+onready var thermal_buffer = stat_thermal_buffer
+onready var temperature = 0
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	$EngineTarget.position.y = -engine_strength
+	$ThrusterTarget.position.x = thruster_strength
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _integrate_forces(state):
+	self.applied_force = Vector2()
+	
+	#handle input
+	#direction assumes ship is looking up, along -y
+	var direction = Vector2()
+	if(Input.is_action_pressed("player_forward")):
+		direction.y -= 1
+	if(Input.is_action_pressed("player_brake")):
+		direction.y += 1
+	if(Input.is_action_pressed("player_turn_left")):
+		direction.x -= 1
+	if(Input.is_action_pressed("player_turn_right")):
+		direction.x += 1
+	
+	#handle reactions
+	if(direction.y < 0):
+		self.fire_engine(direction.y)
+	if(direction.x !=0):
+		fire_thruster(direction.x)
+
+func fire_engine(strength):
+	var pos = ($EngineTarget.global_position - self.global_position)
+	self.applied_force= pos * -1
+	self.temperature += engine_heat
+
+func fire_thruster(strength):
+	#expects a value from -1 to 1
+	self.angular_velocity += strength * thruster_strength / 500
+	
