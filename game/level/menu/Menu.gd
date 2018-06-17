357a95c5c4ae6a193772f49e262cc70e315fabaa
unwarysage <unwary@comcast.net>
2018-06-17 15:30:25 -0500

    first draft of main menu

---
+++ game/level/menu/Menu.gd
@@ -0,0 +1,18 @@
+extends HBoxContainer
+
+
+func _on_NextLevelButton_pressed():
+	pass # replace with function body
+
+
+func _on_RetireButton_pressed():
+	pass # replace with function body
+
+
+func _on_HelpButton_pressed():
+	pass # replace with function body
+
+
+func _on_ExitButton_pressed():
+	print("exiting")
+	SceneTree.quit()
